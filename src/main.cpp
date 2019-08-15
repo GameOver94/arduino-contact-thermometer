278c4025a7b119f0c6de26c985a8941a10efcfe7
Patrick Harrer <patrick.harrer94@hotmail.com>
2019-08-15 13:31:37 +0200

    implementing basic LCD

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,48 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_RGBLCDShield.h>
+#include <utility/Adafruit_MCP23017.h>
+
+
+// initialize the LCD
+Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
 void setup() {
-  // put your setup code here, to run once:
+  // Debugging output
+  Serial.begin(115200);
+  // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+  lcd.setBacklight(0x7);
+  lcd.print("Hello, world!");
 }
 
+uint8_t i=0;
 void loop() {
-  // put your main code here, to run repeatedly:
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis()/1000);
+
+  uint8_t buttons = lcd.readButtons();
+
+  if (buttons) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    if (buttons & BUTTON_UP) {
+      lcd.print("UP ");
+    }
+    if (buttons & BUTTON_DOWN) {
+      lcd.print("DOWN ");
+    }
+    if (buttons & BUTTON_LEFT) {
+      lcd.print("LEFT ");
+    }
+    if (buttons & BUTTON_RIGHT) {
+      lcd.print("RIGHT ");
+    }
+    if (buttons & BUTTON_SELECT) {
+      lcd.print("SELECT ");
+    }
+  }
 }
