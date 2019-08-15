2979b617c1790d238b03523d6e3f10ac8d8cf2b1
Patrick Harrer <patrick.harrer94@hotmail.com>
2019-08-15 19:05:42 +0200

    added PID controll

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,13 @@
 #include <Adafruit_RGBLCDShield.h>
 #include <utility/Adafruit_MCP23017.h>
 
+#include <PID_v1.h>
+
 
 /* --------------- initialize the LCD --------------- */
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 uint8_t degree[8] = {0x7,0x5,0x7,0x0,0x0,0x0,0x0,0x0};
+
 
 /* --------------- initialize DS18B20 --------------- */
 // Data wire is connected to GPIO32
@@ -18,6 +21,23 @@ uint8_t degree[8] = {0x7,0x5,0x7,0x0,0x0,0x0,0x0,0x0};
 OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature sensor 
 DallasTemperature sensors(&oneWire);
+double sensVal;
+
+
+/* ---- initialize PID Controller */
+#define RELAY_PIN 3
+
+//Define Variables we'll be connecting to
+double Setpoint, Output;
+
+//Specify the links and initial tuning parameters
+double Kp=2, Ki=5, Kd=1;
+uint16_t sampleTime = 700;
+PID myPID(&sensVal, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+uint16_t WindowSize = 10000;
+uint32_t windowStartTime;
+
 
 
 
@@ -34,18 +54,69 @@ void setup() {
 
   // Start up the DS18B20 library
   sensors.begin();
+
+
+  // PID
+  pinMode(RELAY_PIN,OUTPUT);
+
+  windowStartTime = millis();
+  //initialize the variables we're linked to
+  Setpoint = 100;
+
+  //tell the PID to range between 0 and the full window size
+  myPID.SetOutputLimits(0, WindowSize);
+  myPID.SetSampleTime(sampleTime);                                                
+
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
+
 }
 
 uint8_t i=0;
-uint32_t lastMsg;
+uint32_t lastMsg = millis();
+uint32_t sensPoll = millis();
 
 void loop() {
+  /* ---------- read Temperature sensor ---------- */
+   if ((millis() - sensPoll > sampleTime) || (millis()-sensPoll < 0)) {     // intervall 5 seconds
+    sensPoll = millis();
+
+    // call sensors.requestTemperatures() to issue a global temperature 
+    // request to all devices on the bus
+    //Serial.print("Requesting temperatures...");
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    sensVal = sensors.getTempCByIndex(0);
+    //Serial.println("DONE");
+    //Serial.print("Temperature for the device 1 (index 0) is: ");
+    //Serial.println(sensVal);
+    Serial.println(Output);
+  }
+
+
+
+  /* ---------- handle PID ---------- */
+  myPID.Compute();
+  /************************************************
+   * turn the output pin on/off based on pid output
+   ************************************************/
+  if (millis() - windowStartTime > WindowSize)
+  { //time to shift the Relay Window
+    windowStartTime += WindowSize;
+  }
+  if (Output < millis() - windowStartTime) digitalWrite(RELAY_PIN, HIGH);
+  else digitalWrite(RELAY_PIN, LOW);
+
+
+
+  /* ---------- update Secons counter on LCD ---------- */
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
   lcd.print(millis()/1000);
 
+
+  /* ---------- hnadle LCD buttons ---------- */
   uint8_t buttons = lcd.readButtons();
 
   if (buttons) {
@@ -68,25 +139,15 @@ void loop() {
     }
   }
 
-// ------------------------------ Non Blocking Sensor Read and LCD Display ------------------------------
+/* ------------------------------ Non Blocking LCD Update ------------------------------ */
   if ((millis() - lastMsg > 5000) || (millis()-lastMsg < 0)) {     // intervall 5 seconds
     lastMsg = millis();
 
-  // call sensors.requestTemperatures() to issue a global temperature 
-  // request to all devices on the bus
-  Serial.print("Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  Serial.println("DONE");
-  // After we got the temperatures, we can print them here.
-  // We use the function ByIndex, and as an example get the temperature from the first sensor only.
-  Serial.print("Temperature for the device 1 (index 0) is: ");
-  Serial.println(sensors.getTempCByIndex(0));
-
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(sensors.getTempCByIndex(0));
-  lcd.write(0);
-  lcd.print("C");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(sensors.getTempCByIndex(0));
+    lcd.write(0);
+    lcd.print("C");
   }
 
 }
